183373d673b5b2578f81aa582416a91997371062
David <ftiqgames@gmail.com>
2013-04-24 21:59:12 -0500

    added project

---
+++ Game State Management Example v1.0/GameStateManagementSample/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SynapseGaming.LightingSystem.Rendering;
+
+namespace GameStateManagementSample
+{
+    class Player
+    {
+        
+
+        public void Initialize()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+        }
+    }
+}
